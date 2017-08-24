f32738e52204838bae5e5401041f31e8a923534b
tdnshah <sankettejas@gmail.com>
2017-08-24 13:14:56 +0530

    state level data object added and works well

--- clix-d3.js
+++ clix-d3.js
@@ -20,25 +20,53 @@ d3.csv("./IMT/DATA/NEWIMTENGLISH25May2017_results-3.csv",function(d){
                     .key(function(d){return d.CLIx_code;})                
                     .key(function(d){return d.survey_time;})
                     .entries(d);
-    nestedData.forEach(function(state_level){  
+//  State Level Nesting happens here 
+    nestedData.forEach(function(state_level){
         var data = {};
         var state_level_implemented_school=0;
         var state_level_not_implemented_school=0;
         var state_level_no_data_available_school=0;
         var district_name;
-        state_level.values.forEach(function(district_level){
-            state_level_no_data_available_school = state_level_no_data_available_school + district_level.values.length
-            district_name = district[district_level.key-1];
-            district_level.values.forEach(function(implemented_school_count){
-                implemented_school_count.values.forEach(function(implemented_school_count_final){
-                    console.log(implemented_school_count_final.values[])
-                })
-            })
-        })
-        
-        Stdata = {state : states[state_level.key-1], noOfImplementedSchools:10, noOfNotImplementedSchools: 2,noDataAvailabeSchools : statesTotalSchools[state_level.key-1]-state_level_no_data_available_school}
-//        console.log(Stdata);
 //        console.log(state_level);
+        
+//  District Level Nesting happens here 
+        state_level.values.forEach(function(district_level){
+            
+            state_level_no_data_available_school = state_level_no_data_available_school + district_level.values.length
+            
+            console.log(district_level);
+            
+            district_name = district[district_level.key-1];
+            
+                district_level.values.forEach(function(implemented_school_count){
+                
+                    console.log(implemented_school_count.values[implemented_school_count.values.length - 1]);
+                    
+                    if(implemented_school_count.values[implemented_school_count.values.length - 1].values[0]["CLIxModule_Impl"]== 1 ){
+                        
+                        state_level_implemented_school ++ ;
+                        
+                       }
+                    else if (implemented_school_count.values[implemented_school_count.values.length - 1].values[0]["CLIxModule_Impl"]== 2){
+                        state_level_not_implemented_school ++;
+                    }
+//                    implemented_school_count.values.forEach(function(implemented_school_count_final){
+//                    
+////                        console.log(implemented_school_count_final.values)
+//                
+//                    });
+                });
+            console.log(state_level_implemented_school)
+            
+        });
+        
+        
+        
+        
+        
+//      final object that is to be sent to d3 visulisation code         
+        Stdata = {state : states[state_level.key-1], noOfImplementedSchools:state_level_implemented_school, noOfNotImplementedSchools: state_level_not_implemented_school,noDataAvailabeSchools : statesTotalSchools[state_level.key-1]-state_level_no_data_available_school}
+        console.log(Stdata);
         
     })
                 
