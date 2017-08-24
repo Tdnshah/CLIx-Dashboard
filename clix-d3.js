16cb8fa24093358f7530bf959a9dbf05e8533d23
tdnshah <sankettejas@gmail.com>
2017-08-24 15:13:17 +0530

    bug of the else condition is fixed

--- clix-d3.js
+++ clix-d3.js
@@ -6,7 +6,7 @@ var district =["Dhamtari","Bilaspur","Aizawl","Baran","Jaipur","Jhalawar","Siroh
 var final_state_level_data = [];
 
 
-d3.csv("./IMT/DATA/NEWIMTENGLISH25May2017_results-3.csv",function(d){
+d3.csv("./IMT/DATA/NEWIMTENGLISH25May2017_results(4).csv",function(d){
     //here the first "d" contains an array, this array contains objects of rows of the csv file
     
     // hence we will nest this objects in the following fashion for us to get the data in proper heirarchy.
@@ -34,42 +34,34 @@ d3.csv("./IMT/DATA/NEWIMTENGLISH25May2017_results-3.csv",function(d){
             
             state_level_no_data_available_school = state_level_no_data_available_school + district_level.values.length
             
-            console.log(district_level);
+//            console.log(district_level);
             
             district_name = district[district_level.key-1];
             
                 district_level.values.forEach(function(implemented_school_count){
                 
-                    console.log(implemented_school_count.values[implemented_school_count.values.length - 1]);
+//                    console.log(implemented_school_count.values[implemented_school_count.values.length - 1]);
                     
                     if(implemented_school_count.values[implemented_school_count.values.length - 1].values[0]["CLIxModule_Impl"]== 1 ){
                         
                         state_level_implemented_school ++ ;
                         
-                       }
+                        }
                     else if (implemented_school_count.values[implemented_school_count.values.length - 1].values[0]["CLIxModule_Impl"]== 2){
                         state_level_not_implemented_school ++;
-                    }
+                        }
 //                    implemented_school_count.values.forEach(function(implemented_school_count_final){
 //                    
 ////                        console.log(implemented_school_count_final.values)
 //                
 //                    });
                 });
-            console.log(state_level_implemented_school)
-            
         });
-        
-        
-        
-        
-        
 //      final object that is to be sent to d3 visulisation code         
         Stdata = {state : states[state_level.key-1], noOfImplementedSchools:state_level_implemented_school, noOfNotImplementedSchools: state_level_not_implemented_school,noDataAvailabeSchools : statesTotalSchools[state_level.key-1]-state_level_no_data_available_school}
-        console.log(Stdata);
-        
+        final_state_level_data.push(Stdata);
     })
-                
+console.log(final_state_level_data);                
 })
 
 
